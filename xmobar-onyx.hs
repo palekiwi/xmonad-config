f3e45c41457a15d64785a48d5c2ffe3188161281
Pawel Lisewski <pawel.lisewski@gmail.com>
2018-06-22 18:32:57 +0800

    adjust xmobar for onyx

--- xmobar-onyx.hs
+++ xmobar-onyx.hs
@@ -15,9 +15,9 @@ Config {
     -- right_padding = num_icons * icon_size
     -- right_padding = 12 * 23 = 276
     -- Example: position = TopP 0 276
-    position = TopP 0 0,
-    font = "xft:SFNS Display:size=6,FontAwesome:size=6",
-    --font = "xft:ubuntu-6",
+    position = TopP 0 100,
+    font = "xft:SFNS Display:size=9,FontAwesome:size=9",
+    --font = "xft:ubuntu-10",
     --additionalFonts = [ "xft:FontAwesome:pixelsize=13" ],
     bgColor = "#ffffff",
     fgColor = "#000000",
