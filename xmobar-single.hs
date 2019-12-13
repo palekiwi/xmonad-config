04636675a3b09dc7f8094dd590d739c267d4ed4f
Pawel Lisewski <pawel.lisewski@gmail.com>
2019-12-13 18:47:50 +0800

    use aggregate CPU usage

--- xmobar-single.hs
+++ xmobar-single.hs
@@ -29,7 +29,7 @@ Config {
     persistent = True,
     commands = [
         --Run Weather "KPAO" ["-t","<tempF>F <skyCondition>","-L","64","-H","77","-n","#CEFFAC","-h","#FFB6B0","-l","#96CBFE"] 36000,
-        Run MultiCpu ["-t","<autototal>", "-w", "2" , "-c", " ","-L","30","-H","60","-h","#EB484F","-l","#5DE489","-n","#BABDB6"] 10,
+        Run MultiCpu ["-t"," <total>%", "-m","2","-c", " ","-L","30","-H","60","-h","#EB484F","-l","#5DE489","-n","#BABDB6"] 10,
         Run Memory ["-t"," <usedratio>%","-H","8192","-L","4096","-h","#EB484F","-l","#5DE489","-n","#BABDB6"] 10,
         --Run Swap ["-t","Swap: <usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
         --Run Network "eth0" ["-t","Net: <rx>, <tx>","-H","200","-L","10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10,
