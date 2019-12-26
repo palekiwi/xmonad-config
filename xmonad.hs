ba8de0f25ea2226ce6263b81770ccc0460221350
Pawel Lisewski <pawel@lisewski.gmail.com>
2019-12-26 15:07:08 +0800

    remove pomodoro

--- xmonad.hs
+++ xmonad.hs
@@ -25,7 +25,7 @@ import qualified Data.Map        as M
 -- The preferred terminal program, which is used in a binding below and by
 -- certain contrib modules.
 --
-myTerminal = "/usr/bin/urxvt"
+myTerminal = "urxvt"
 
 -- The command to lock the screen or show the screensaver.
 myScreensaver = "/usr/bin/xscreensaver-command -l"
